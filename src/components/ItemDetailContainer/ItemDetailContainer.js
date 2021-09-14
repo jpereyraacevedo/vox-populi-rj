ebf6ce6ceb6203473f29c35d8cc3ff5e4c00bdbb
Jonatan Pereyra Acevedo <jpereyraacevedo@gmail.com>
2021-09-14 14:35:04 -0300

    firebase fixed

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,7 +3,7 @@ import { Spinner } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import { ItemDetail } from './ItemDetail'
 import { funcionObtenerDatos } from '../../utilities/funcionObtenerDatos'
-
+import { getFirestore } from '../../firebase/firebase'
 
 export const ItemDetailContainer = () => {
 
@@ -14,16 +14,65 @@ export const ItemDetailContainer = () => {
 
     useEffect(() => {
 
-        setLoading (true)
+        setLoading(true);
+  
+  
+  
+        const db = getFirestore();
+  
+        let producto = db.collection("productos").doc(itemId); // es basicamente igual al de ItemListContainer pero le agregamos el .doc( id del documento ) para traer un documento en particular
+  
+  
+  
+        producto
+  
+           .get()
+  
+           .then((response) => {
+  
+              setItem({...response.data(),    // aca en vez de hacer el map le pasamos directamente el objeto que trajimos
+  
+                 id: response.id,          // podrias tener logica para que si ingresan un id por url de algo que no existe en firebase muestrea algun error o redirijas a la home
+  
+              });
+  
+           })
+  
+           .catch((err) => console.log(err))
+  
+           .finally(() => {
+  
+              setLoading(false);
+  
+           });
+  
+           
+  
+     }, [itemId]);
 
-        funcionObtenerDatos()
-            .then (res => {
-                setItem( res.find (prod => prod.id === parseInt(itemId)))
-            })
-            .finally(()=> {
-                setLoading(false)
-            })
-    }, [itemId])
+
+    // Funcion previa correccion Gabriel
+    // useEffect(() => {
+
+    //     setLoading (true)
+
+    //     const db = getFirestore()
+    //     const productos = db.collection('productos')
+    //     const items = productos.doc(itemId)
+    //     items.get().then((documento) => {
+    //         setItem({...documento.data(), id: documento.id})
+    //     }).finally(()=> {
+    //         setLoading(false)})
+
+
+    //     // funcionObtenerDatos()
+    //     //     .then (res => {
+    //     //         setItem( res.find (prod => prod.id === parseInt(itemId)))
+    //     //     })
+    //         // .finally(()=> {
+    //         //     setLoading(false)
+    //         // })
+    // }, [itemId])
 
     return(
         <>
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -14,33 +14,111 @@ export const ItemListContainer = () => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
 
-        useEffect(() => {
+
+    useEffect(() => {
+
+        setLoading(true)
 
 
-            const db= getFirestore()
-            const productos = db.collection('productos')
-            productos.get().then((response) => {
-                const data = response.docs.map((documento) => ({...documento.data(), id: documento.id}))
-                console.log(data)
 
-                setData(data)
-            })
+        const db= getFirestore()
 
-            setLoading(true)
-            funcionObtenerDatos()
-                .then(res => {
-                    if (cat) {
-                        const arrayParaFiltrarCategory = res.filter(prod => prod.category === cat)
-                    setData(arrayParaFiltrarCategory)
-                    } else {
-                        setData(res)
-                    }
-                })
-                .catch(err => console.log(err))
-                .finally (() => {
-                    setLoading(false)
-                })
-        }, [cat])
+        let productos = db.collection('productos')   //hacemos que sea una variable
+
+
+
+        if (cat) {         // si viene la categoria le agregamos el filtro
+
+            productos = productos.where("category", "==", cat)
+
+        }
+
+
+
+        productos.get().then((response) => {  // esto es tal cual lo tenias. le pase el catch y finally solamente
+
+            const data = response.docs.map((documento) => ({...documento.data(), id: documento.id}))
+
+            setData(data)
+
+        })
+
+        .catch(err => console.log(err))
+
+        .finally (() => {
+
+            setLoading(false)
+
+        })
+
+    }, [cat])
+
+        // useEffect(() => {
+
+        // // FUNCION ANTERIOR CORRECION GABRIEL
+
+        //     setLoading(true)
+
+        //     const db= getFirestore()
+        //     let productos = db.collection('productos')   // pasar const a let
+
+        //     if (cat) {
+        //        // const filtro = productos.where('category', '==', cat) ----> filtro anterior
+        //         productos = productos.where("category", "==", cat)
+
+        //         filtro.get()
+        //         .then((response) => {
+        //             const data = response.docs.map((documento) => ({...documento.data(), id: documento.id}))
+        //             console.log(data)
+        //             setData(data)
+        //         })
+        //         .finally (() => {
+        //             setLoading(false)
+        //         })
+        //     } else {
+        //         productos.get()
+        //         .then((response) => {
+        //             const data = response.docs.map((documento) => ({...documento.data(), id: documento.id}))
+        //             console.log(data)
+        //             setData(data)
+        //         })
+        //         .finally (() => {
+        //             setLoading(false)
+        //         })
+        //     }
+
+            // productos.get().then((response) => {
+            //     const data = response.docs.map((documento) => ({...documento.data(), id: documento.id}))
+            //     console.log(data)
+            //     if (cat) {
+            //         setData(data.filter(elementos => elementos.category === cat))
+            //     }
+
+
+            //     setData(data)
+            // }).finally (() => {
+            //     setLoading(false)
+            // })
+
+            // setLoading(true)
+
+            // //FUNCION PARA LOS DATOS
+            // funcionObtenerDatos()
+            //     .then(res => {
+            //         if (cat) {
+            //             const arrayParaFiltrarCategory = res.filter(prod => prod.category === cat)
+            //         setData(arrayParaFiltrarCategory)
+            //         } else {
+            //             setData(res)
+            //         }
+            //     })
+            //     .catch(err => console.log(err))
+            //     .finally (() => {
+            //         setLoading(false)
+            //     })
+
+                // Cuidado con este .finally
+       // }, [cat, setLoading])
 
 
     return (
