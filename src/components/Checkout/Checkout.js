8bd81b16a8c3110266fd708c1846375e04dcdb6d
jpereyraacevedo <jpereyraacevedo@gmail.com>
2021-09-18 14:25:43 -0300

    checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { CartContext } from '../../context/CartContext'
+import { getFirestore } from '../../firebase/firebase'
 
 
 
 export const Checkout = () => {
+
+    // Informacion utilizada del cart del contexto
+    const { cart } = useContext(CartContext)
+
+
+    // Informacion traida del firebase
+    const db = getFirestore()
+    const ordenes = db.collection('ordenes')
+
+
+    // Usuarios compradores
+
+    const comprador = {
+        nombre: 'Jonatan Pereyra',
+        telefono: 3424281330,
+        correo: 'jpereyraacevedo@gmail.com'
+    }
 
     return (
         <>
